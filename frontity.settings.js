211d73b6a3db675669add6a1dcf8f072e0459279
Jozef Pierlejewski <joe@codemonkeynorth.co.uk>
2021-06-14 16:52:57 +0100

    swap head tags for yoast

--- frontity.settings.js
+++ frontity.settings.js
@@ -73,7 +73,7 @@ const settings = {
     },
     "@frontity/tiny-router",
     "@frontity/html2react",
-    "@frontity/head-tags"
+    "@frontity/yoast",
   ]
 };
 
--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -15,7 +15,7 @@ import Events from "./pages/events"
 const Theme = ({ state }) => {
   // Get information about the current URL.
   const data = state.source.get(state.router.link);
-
+  console.log(data)
   return (
     <>
       {/* Add some metatags to the <head> of the HTML. */}
